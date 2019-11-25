f3ae8e91b3f62aa730d4db88915926cb2810d381
sampsota <sampsota@outlook.com>
2019-11-25 16:29:54 -0700

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { HelloComponent, SamComponent } from './hello.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatSliderModule, MatCardModule, MatButtonModule ],
+  imports:      [ BrowserModule, FormsModule, MatSliderModule, MatCardModule, MatButtonModule, MatToolbarModule, MatIconModule ],
   declarations: [ AppComponent, HelloComponent, SamComponent ],
   bootstrap:    [ AppComponent ]
 })
